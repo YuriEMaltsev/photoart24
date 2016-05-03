4c692af1c3a17261da31455ce9927ce48395ea35
Yuri E Maltsev <ye.maltsev@yandex.ru>
2016-05-03 21:20:09 +0700

    first

---
+++ frontend/views/site/service.php
@@ -0,0 +1,14 @@
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Услуги';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p> Заготовка страницы о себе</p>
+
+    <code><?= __FILE__ ?></code>
+</div>
